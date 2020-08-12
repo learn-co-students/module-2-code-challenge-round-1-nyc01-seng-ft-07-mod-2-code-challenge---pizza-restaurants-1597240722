847d8531759e79de00f5f1793d22294f06151756
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-12 10:51:07 -0400

    add non-advanced deliverables

---
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantPizzasController < ApplicationController
+    def new
+        @restaurant_pizza = RestaurantPizza.new
+    end 
+
+    def create
+        @restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+        if @restaurant_pizza.valid?
+            redirect_to restaurant_path(restaurant_pizza_params[:restaurant_id])
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.require(:restaurant_pizza).permit(:restaurant_id, :pizza_id, :price)
+    end
+end
