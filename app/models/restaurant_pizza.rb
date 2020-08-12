3410f9ba3a23f3653b0d7b38f79472353e4cfa42
Mimioj101 <mimioj@gmail.com>
2020-08-12 11:53:39 -0400

    bonus deliverables

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -3,5 +3,5 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant
 
     validates :price, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 30}
-    
+    validates_uniqueness_of :pizza_id, scope: :restaurant_id
 end
