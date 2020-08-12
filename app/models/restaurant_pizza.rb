877cf6834f33d34f9fd62305f6d9ce3217248c12
pairofAces <karanchauhan16@gmail.com>
2020-08-12 11:50:16 -0400

    fixed some things

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -3,5 +3,5 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant
 
     validates :price, numericality: {greater_than: 1, less_than: 30}
-    validates :restaurant_id, uniqueness: true
+    
 end
