4b79321216ee24fb982ddd2e1d3bccd7d546bc0c
junko911 <junkotahara911@gmail.com>
2020-08-12 11:37:38 -0400

    Added RestaurantPizza validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,5 +2,6 @@ class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
 
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 30}
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 30, message: "should be between 1 and 30."}
+  validates :restaurant_id, uniqueness: { scope: :pizza_id, message: "already has this pizza on menu." }
 end
