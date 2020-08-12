8c3926ab55a3670fe74d516cd0c516ca2d7d95c3
github username <github email address>
2020-08-12 11:52:22 -0400

    missing last advanced

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,11 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  # def average
+  #   assoc = RestaurantPizza.all.select{|RP| RP.restaurant == self}
+  #   total = assoc.sum{|inst| inst.price}
+  #   avg = total / assoc.count
+  #   avg
+  # end
 end
