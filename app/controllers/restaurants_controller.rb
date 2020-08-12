f2f2329017355db2d438b20d7fab9ef2b285e658
github username <github email address>
2020-08-12 11:56:01 -0400

    final

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,10 +7,5 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-  # def average
-  #   assoc = RestaurantPizza.all.select{|RP| RP.restaurant == self}
-  #   total = assoc.sum{|inst| inst.price}
-  #   avg = total / assoc.count
-  #   avg
-  # end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :restaurant_pizzas
   has_many :pizzas, through: :restaurant_pizzas
+
+  def average
+    assoc = RestaurantPizza.all.select{|rp| rp.restaurant == self}
+    total = assoc.sum{|inst| inst.price}
+    avg = total / assoc.count
+    avg
+  end
 end
