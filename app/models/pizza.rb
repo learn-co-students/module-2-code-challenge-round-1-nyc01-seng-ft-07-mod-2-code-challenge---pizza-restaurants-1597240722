985d106e10f12819a70973e53db50b4aa9b23f46
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-12 11:25:56 -0400

    Everything except for one validation

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -2,7 +2,15 @@ class Pizza < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :restaurants, through: :restaurant_pizzas
 
+
     def restaurant_count
         self.restaurants.count
+    end
+
+    def pizza_average_price
+        total_price = 0
+        average = 0
+        self.restaurant_pizzas.each {|rp| total_price += rp.price}
+        average = total_price / self.restaurant_pizzas.count
     end
 end
