bb389e50abdf54b8cdd68a9bc374417a5432c429
cdemarti <codythomasdemartin@gmail.com>
2020-08-12 11:31:06 -0400

    Advanced deliverables complete.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,15 @@
 class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :pizzas, through: :restaurant_pizzas
+
+    def get_pizza_prices
+        self.restaurant_pizzas.map do |pizza_r|
+            pizza_r.price 
+        end 
+    end 
+
+    def get_average_price
+        self.get_pizza_prices.sum / self.get_pizza_prices.count
+    end
+
 end
