de24b0c84557ad0809a1980585ad59b433aa282b
redazures <jwu_nyc@yahoo.com>
2020-08-12 10:47:34 -0400

    first set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+  resources :branches
+  resources :restaurants, only: [:show, :index]
+  resources :pizzas, only: [:show, :index]
 end
