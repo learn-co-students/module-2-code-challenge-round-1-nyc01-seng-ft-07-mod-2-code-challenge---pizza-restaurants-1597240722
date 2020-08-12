b8c8dd2ff24c5fe38e1f84a370021c9125ccecb2
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-12 11:42:56 -0400

    Finished

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,6 +2,10 @@ class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
   validates :price, inclusion: {in: 1..30}
-  
-
+  validate :associate_once 
+  def associate_once
+    if RestaurantPizza.all.find_all{|rp|(rp.pizza_id == self.pizza_id) && (rp.restaurant_id == self.restaurant_id)}.count > 1
+      errors.add(:pizza, "already exists in this Restaurant")
+    end
+  end
 end
